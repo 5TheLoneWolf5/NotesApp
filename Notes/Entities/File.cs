bae46edab45e4ba2fd5fef4af69fbfcca45c9dbb
5TheLoneWolf5 <89516641+5TheLoneWolf5@users.noreply.github.com>
2023-12-01 16:43:39 -0300

    First commit.

---
+++ Notes/Entities/File.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Notes
+{
+    class File
+    {
+        private string name;
+        private StreamWriter sw;
+        private StreamReader sr;
+        private bool open;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public bool Open
+        {
+            get => open;
+
+            private set => open = value;
+        }
+
+        public File(string name)
+        {
+            this.name = name;
+        }
+
+        public void CreateOrOpenFile()
+        {
+            sw = new StreamWriter(name + ".txt", true, System.Text.Encoding.UTF8);
+            open = true;
+        }
+
+        public void CloseFile()
+        {
+            sw.Close();
+            open = false;
+        }
+
+        public void RecordMessage(string msg)
+        {
+            sw.WriteLine(msg);
+        }
+
+        public void ReadFile()
+        {
+            Console.WriteLine();
+
+            try {
+
+                sr = new StreamReader(name+".txt");
+                string line = sr.ReadLine();
+
+                while (!String.IsNullOrWhiteSpace(line))
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+
+                sr.Close(); // It opens and closes StreamReader inside the method.
+
+            } catch (System.IO.IOException e)
+            {
+                Console.WriteLine("Written file is stil open and needs closing!\n" + e.Message);
+                this.CloseFile();
+            }
+
+        }
+
+    }
+}
---
+++ Notes/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("--- Notes App ---");
+            Console.WriteLine("\nType the file name to open or create one: ");
+
+            File file = new File(Console.ReadLine());
+            file.CreateOrOpenFile();
+            file.CloseFile(); // Starting at 0. If user wants to only read file, for example.
+
+            int userInput = 0;
+
+            while(userInput != 3)
+            {
+                Console.WriteLine("\n1 - Type a note (type EXIT to leave)\n2 - Read notes\n3 - Leave\n");
+                userInput = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                
+                switch (userInput)
+                {
+                    case 1:
+
+                        if(!file.Open)
+                        {
+                            file.CreateOrOpenFile();
+                        }
+
+                        string userInput2 = "";
+                        int currentLine = 0;
+
+                        do
+                        {
+                            currentLine++;
+                            Console.WriteLine($"Note {currentLine}: ");
+                            userInput2 = Console.ReadLine();
+                            if (!userInput2.Trim().ToUpper().Equals("EXIT"))
+                                file.RecordMessage(userInput2);
+                        } while(!userInput2.Trim().ToUpper().Equals("EXIT"));
+
+                        file.CloseFile();
+                        break;
+                    case 2:
+                        file.ReadFile();
+                        break;
+                    case 3:
+                        Console.WriteLine("Exiting program.");
+                        break;
+                    default:
+                        Console.WriteLine("Command not identified.");
+                        break;
+                }
+            }
+
+        }
+    }
+}
